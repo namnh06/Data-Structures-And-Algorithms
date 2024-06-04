61c48eebdd589eed26924c78d0b5ec0cd2813a3e
Nam H Nguyen <namnh.developer@gmail.com>
2024-06-03 23:24:30 -0600

    [Python] Added 136 Single Number

---
+++ Python/Array/136-Single-Number/main.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/single-number/
+
+class Solution:
+    def singleNumber(self, nums: list[int]) -> int:
+        nums.sort() # Time Complexity: O(n log n)
+        n: int = len(nums)
+        if n == 1:
+            return nums[0]
+        i: int = 1
+        while i < n: # Time Complexity: O(n)
+            if nums[i] != nums[i-1]:
+                return nums[i-1]
+            i += 2
+            if i >= n-1:
+                return nums[n-1]
+
+        # Space Complexity: O(1)
+        
+if __name__ == '__main__':
+    s = Solution()
+    
+    result = s.singleNumber([2,2,1])
+    assert result == 1
+    
+    result = s.singleNumber([1,0,1])
+    assert result == 0
