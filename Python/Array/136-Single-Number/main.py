58cd9d29b7d6d9168b85821f9131596db2455e75
Nam H Nguyen <namnh.developer@gmail.com>
2024-06-04 10:03:06 -0600

    [Python] Changed 136 Single Number XOR solution

--- Python/Array/136-Single-Number/main.py
+++ Python/Array/136-Single-Number/main.py
@@ -2,19 +2,23 @@
 
 class Solution:
     def singleNumber(self, nums: list[int]) -> int:
-        nums.sort() # Time Complexity: O(n log n)
-        n: int = len(nums)
-        if n == 1:
-            return nums[0]
-        i: int = 1
-        while i < n: # Time Complexity: O(n)
-            if nums[i] != nums[i-1]:
-                return nums[i-1]
-            i += 2
-            if i >= n-1:
-                return nums[n-1]
-
+        # n: int = len(nums)
+        # nums.sort() # Time Complexity: O(n log n)
+        # if n == 1:
+        #     return nums[0]
+        # i: int = 1
+        # while i < n: # Time Complexity: O(n)
+        #     if nums[i] != nums[i-1]:
+        #         return nums[i-1]
+        #     i += 2
+        #     if i >= n-1:
+        #         return nums[n-1]
         # Space Complexity: O(1)
+        
+        result: int = 0
+        for num in nums:
+            result ^= num
+        return result
         
 if __name__ == '__main__':
     s = Solution()
